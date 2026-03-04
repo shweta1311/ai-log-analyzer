4d5a60e5d07b19a92eaaaa1463fc12c93dc9809c
shweta1311 <shweta.yadav.1311@gmail.com>
2026-03-04 23:08:29 +0530

    Initial Spring Boot project setup

---
+++ ai-log-analyzer/src/main/java/com/shweta/ai_log_analyzer/AiLogAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.shweta.ai_log_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLogAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLogAnalyzerApplication.class, args);
+	}
+
+}
---
+++ ai-log-analyzer/src/test/java/com/shweta/ai_log_analyzer/AiLogAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shweta.ai_log_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLogAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
